6dadddc2dad5771305dab15194c32668b807fd21
Viet Q. Nguyen <quangvietnguyen@Viets-Mac.local>
2021-04-14 00:49:29 -0400

    init

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import classes from './Burger.module.css';
+
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = ( props ) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            
+        });
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="salad"/>
+            <BurgerIngredient type="meat"/>
+            <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+};
+
+export default burger;
